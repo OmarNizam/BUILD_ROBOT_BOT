eb5b958e1fff8248b03e593781819e264374e344
OmarNizam <eng.omarnizam@gmail.com>
2020-07-28 21:10:20 +0200

    init command

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -32,13 +32,12 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'HomePage',
   props: {
     msg: String,
   },
 };
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
